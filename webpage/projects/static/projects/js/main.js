dd6914be176686e7e66c9fd2660fa3e41584cc38
Andrew Caietti <67521582+caiettia@users.noreply.github.com>
2025-01-29 19:11:32 -0500

    updating mobile styling for nav slection

--- webpage/projects/static/projects/js/main.js
+++ webpage/projects/static/projects/js/main.js
@@ -1,10 +1,18 @@
-// your_app/static/your_app/js/main.js
 document.addEventListener('DOMContentLoaded', function() {
     const navToggle = document.querySelector('.nav-toggle');
     const navMenu = document.querySelector('.nav-menu');
 
-    navToggle.addEventListener('click', function() {
-        navMenu.classList.toggle('show');
-    });
+    // navToggle.addEventListener('click', function() {
+    //     navMenu.classList.toggle('show');
+    // });
 
+    navToggle.addEventListener('click', function() {
+        if ( navMenu.classList.contains('nav-menu')) {
+            navMenu.classList.remove('nav-menu');
+            navMenu.classList.add('nav-menu-show');
+        } else {
+            navMenu.classList.remove('nav-menu-show');
+            navMenu.classList.add('nav-menu');
+        }
+    });
 });
